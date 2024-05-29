8f185c7c4e578224bfc857b193c850aac6f68dc7
Chiranthchiru65 <chiranthchiru65@gmail.com>
2024-05-29 18:36:16 +0530

    latest working of game

--- App.jsx
+++ App.jsx
@@ -12,34 +12,36 @@ function App() {
 export default App;
 
 function GameBoard() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="container">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} />
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <div className="cell" onClick={handleClick}>
+    <div className="cell" onClick={onSquareClick}>
       {value}
     </div>
   );
